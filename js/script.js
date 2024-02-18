2695249bd8956118e04bb308e103fc8c8807d75e
Mehedi Hasan <mhedih32@gmail.com>
2024-02-18 19:15:21 +0600

    apply button work content added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@ function modalSection(){
     // console.log('Mehedi Hasan is the best man in bangladesh.')
     // step--1: hide the all section and
     const mainSection=document.getElementById('main-section');
-    mainSection.classList.add('hidden')
+    mainSection.classList.add('hidden');
     // step--2: Show the done section
     const doneSection=document.getElementById('modal');
     doneSection.classList.remove('hidden')
+}
+// apply button function
+document.getElementById('delete-confirm').addEventListener('keyup',function(event){
+    const text = event.target.value;
+    const deleteButton=document.getElementById('apply-button');
+    if(text==='NEW15'||text==='Couple 20'){
+        deleteButton.removeAttribute('disabled')
+    }else{
+        deleteButton.setAttribute('disabled',true)
+    }
+})
+// apply button hidden function
+function applyButton(){
+    const applyButtonSection=document.getElementById('apply-button');
+    applyButtonSection.classList.add('hidden');
 }
